a9d19f4ce680d99d9eb92a845691479b2feafc6e
horothesun <horothesun@gmail.com>
2023-12-02 15:19:36 +0000

    Day 2: part 2

--- src/main/scala/Day2.scala
+++ src/main/scala/Day2.scala
@@ -27,15 +27,38 @@ object Validity:
     def empty: Validity = Possible
     def combine(x: Validity, y: Validity): Validity = if (x == Impossible || y == Impossible) Impossible else Possible
 
-case class Bag(cubesByColor: Map[Color, Int])
+case class Bag(cubesByColor: Map[Color, Int]):
+  def getCubes(c: Color): Int = cubesByColor.getOrElse(c, 0)
+
+  def power: Int = Color.values.map(getCubes).toList.product
+
 object Bag:
+  val empty: Bag = Bag(cubesByColor = Color.values.toList.map(_ -> 0).toMap)
   val part1: Bag = Bag(cubesByColor = Map(Red -> 12, Green -> 13, Blue -> 14))
 
+  val maxMonoid: Monoid[Bag] =
+    def max(c: Color, b1: Bag, b2: Bag): Int = Math.max(b1.getCubes(c), b2.getCubes(c))
+    Monoid.instance(
+      emptyValue = Bag.empty,
+      cmb = (b1, b2) =>
+        Bag(cubesByColor =
+          Map(
+            Red -> max(Red, b1, b2),
+            Green -> max(Green, b1, b2),
+            Blue -> max(Blue, b1, b2)
+          )
+        )
+    )
+
 case class Reveal(cubesByColor: Map[Color, Int]):
+  def getCubes(c: Color): Int = cubesByColor.getOrElse(c, 0)
+
   def validity(bag: Bag): Validity =
     def isPossibleByColor(c: Color): Validity =
       if (cubesByColor.getOrElse(c, 0) <= bag.cubesByColor.getOrElse(c, 0)) Possible else Impossible
     Color.values.toList.foldMap(isPossibleByColor)
+
+  def fewestCubes: Bag = Bag(cubesByColor = Color.values.toList.map(c => c -> getCubes(c)).toMap)
 
 object Reveal:
   def from(s: String): Option[Reveal] =
@@ -49,6 +72,8 @@ object Reveal:
 
 case class Game(id: GameId, reveals: List[Reveal]):
   def validity(bag: Bag): Validity = reveals.foldMap(r => r.validity(bag))
+
+  def fewestCubes: Bag = reveals.foldMap(_.fewestCubes)(Bag.maxMonoid)
 
 object Game:
   def from(s: String): Option[Game] =
@@ -65,3 +90,7 @@ def getGames(input: List[String]): Option[List[Game]] = input.traverse(Game.from
 def getPossibleGameIdsSum(games: List[Game]): Int = games.filter(_.validity(Bag.part1) == Possible).map(_.id).sum[Int]
 
 def getPossibleGameIdsSum(input: List[String]): Option[Int] = getGames(input).map(getPossibleGameIdsSum)
+
+def getGameFewestCubesPowerSum(games: List[Game]): Int = games.map(_.fewestCubes.power).sum
+
+def getGameFewestCubesPowerSum(input: List[String]): Option[Int] = getGames(input).map(getGameFewestCubesPowerSum)
--- src/test/scala/Day2Suite.scala
+++ src/test/scala/Day2Suite.scala
@@ -3,16 +3,9 @@ import Color.*
 import Validity.*
 import Day2Suite.*
 
-class Day2Suite extends ScalaCheckSuite {
+class Day2Suite extends ScalaCheckSuite:
 
   test("getGames(smallInput) returns correct value") {
-    val smallInput = List(
-      "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-      "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-      "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-      "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-      "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
-    )
     assertEquals(getGames(smallInput), Some(List(game1, game2, game3, game4, game5)))
   }
 
@@ -37,13 +30,6 @@ class Day2Suite extends ScalaCheckSuite {
   }
 
   test("getPossibleGameIdsSum(smallInput) == Some(8)") {
-    val smallInput = List(
-      "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
-      "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
-      "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
-      "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
-      "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
-    )
     assertEquals(getPossibleGameIdsSum(smallInput), Some(8))
   }
 
@@ -51,9 +37,43 @@ class Day2Suite extends ScalaCheckSuite {
     assertEquals(getPossibleGameIdsSum(bigInput), Some(2101))
   }
 
-}
-object Day2Suite {
+  test("game1.fewestCubes == 4 red, 2 green, and 6 blue") {
+    assertEquals(game1.fewestCubes, Bag(cubesByColor = Map(Red -> 4, Green -> 2, Blue -> 6)))
+  }
 
+  test("game2.fewestCubes == 1 red, 3 green, and 4 blue") {
+    assertEquals(game2.fewestCubes, Bag(cubesByColor = Map(Red -> 1, Green -> 3, Blue -> 4)))
+  }
+
+  test("game3.fewestCubes == 20 red, 13 green, and 6 blue") {
+    assertEquals(game3.fewestCubes, Bag(cubesByColor = Map(Red -> 20, Green -> 13, Blue -> 6)))
+  }
+
+  test("game4.fewestCubes == 14 red, 3 green, and 15 blue") {
+    assertEquals(game4.fewestCubes, Bag(cubesByColor = Map(Red -> 14, Green -> 3, Blue -> 15)))
+  }
+
+  test("game5.fewestCubes == 6 red, 3 green, and 2 blue") {
+    assertEquals(game5.fewestCubes, Bag(cubesByColor = Map(Red -> 6, Green -> 3, Blue -> 2)))
+  }
+
+  test("getGameFewestCubesPowerSum(smallInput) == Some(2286)") {
+    assertEquals(getGameFewestCubesPowerSum(smallInput), Some(2286))
+  }
+
+  test("getGameFewestCubesPowerSum(bigInput) == Some(58_269)") {
+    assertEquals(getGameFewestCubesPowerSum(bigInput), Some(58_269))
+  }
+
+object Day2Suite:
+
+  val smallInput: List[String] = List(
+    "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+    "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+    "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+    "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+    "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green"
+  )
   val bigInput: List[String] = getLinesFromFile("src/test/scala/day2_input.txt")
 
   val game1: Game = Game(
@@ -95,5 +115,3 @@ object Day2Suite {
       Reveal(Map(Blue -> 2, Red -> 1, Green -> 2))
     )
   )
-
-}
