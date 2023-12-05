274129f19460cd3cfb12bc329a98c4a519bc0d94
horothesun <horothesun@gmail.com>
2023-12-05 11:38:55 +0000

    Day 3: part 1

---
+++ src/main/scala/Day3.scala
@@ -0,0 +1,70 @@
+import cats.derived.*
+import cats.implicits.*
+import cats.{Functor, Traverse}
+
+val DOT_CHAR = '.'
+
+case class Symbol(c: Char)
+object Symbol:
+  def from(c: Char): Option[Symbol] = if (c == DOT_CHAR || c.isDigit) None else Some(Symbol(c))
+
+case class PartNumber(n: Int)
+object PartNumber:
+  def from(s: String): Option[PartNumber] = s.toIntOption.map(PartNumber.apply)
+
+case class Part(s: Symbol, pn: PartNumber)
+
+case class Annotated[+A](a: A, pos0Based: Int, length: Int) derives Functor, Traverse
+
+type Literal = Symbol | PartNumber
+
+def parseRow(s: String): Option[List[Annotated[Literal]]] =
+  val (lastPartNumber, optLits) = s.toList.zipWithIndex
+    .foldLeft[(Option[Annotated[String]], List[Option[Annotated[Literal]]])]((None, List.empty)) {
+      case ((nextPartNumber, acc), (c, pos)) =>
+        c match
+          case DOT_CHAR =>
+            nextPartNumber.fold(ifEmpty = (None, acc))(npn => (None, acc :+ npn.traverse(PartNumber.from)))
+          case d if d.isDigit =>
+            val annotatedDigit = Annotated(s"$d", pos, length = 1)
+            nextPartNumber.fold(ifEmpty = (Some(annotatedDigit), acc)) { npn =>
+              val newNpn = Annotated(s"${npn.a}$d", pos0Based = npn.pos0Based, length = 1 + npn.length)
+              (Some(newNpn), acc)
+            }
+          case sym =>
+            val symLit = Some(Annotated[Literal](Symbol(sym), pos, length = 1))
+            nextPartNumber.fold(ifEmpty = (None, acc :+ symLit))(npn =>
+              (None, acc :+ npn.traverse(PartNumber.from) :+ symLit)
+            )
+    }
+  optLits.sequence.map(ls => lastPartNumber.flatMap(_.traverse(PartNumber.from)).fold(ifEmpty = ls)(ls :+ _))
+
+def parse(input: List[String]): Option[List[List[Annotated[Literal]]]] = input.traverse(parseRow)
+
+val annotatedSymbol: PartialFunction[Annotated[Literal], Annotated[Symbol]] =
+  case Annotated(Symbol(c), p, l) => Annotated(Symbol(c), p, l)
+
+val annotatedPartNumber: PartialFunction[Annotated[Literal], Annotated[PartNumber]] =
+  case Annotated(PartNumber(n), p, l) => Annotated(PartNumber(n), p, l)
+
+def getMatchingPart(apn: Annotated[PartNumber], as: Annotated[Symbol]): Option[Part] =
+  if (
+    as.pos0Based >= apn.pos0Based - 1 &&
+    as.pos0Based <= apn.pos0Based + apn.length
+  ) Some(Part(as.a, apn.a))
+  else None
+
+def getParts(rows: List[List[Annotated[Literal]]]): List[Part] =
+  val emptyRow = List.empty[Annotated[Literal]]
+  val prevCurrNextRows =
+    (emptyRow :: rows)
+      .zip(rows)
+      .zip(rows.drop(1) :+ emptyRow)
+      .map { case ((p, c), n) => (p, c, n) }
+  prevCurrNextRows.flatMap { case (prev, curr, next) =>
+    curr.collect(annotatedPartNumber).flatMap { apn =>
+      List(prev, curr, next).flatMap(_.collect(annotatedSymbol).mapFilter[Part](getMatchingPart(apn, _)))
+    }
+  }
+
+def getPartNumbersSum(input: List[String]): Option[Int] = parse(input).map(rows => getParts(rows).map(_.pn.n).sum)
---
+++ src/test/scala/Day3Suite.scala
@@ -0,0 +1,92 @@
+import munit.ScalaCheckSuite
+import org.scalacheck.Gen
+import org.scalacheck.Prop.*
+import Day3Suite.*
+
+class Day3Suite extends ScalaCheckSuite:
+
+  test("parse(smallInput) returns the correct value") {
+    assertEquals(parse(smallInput), Some(smallRows))
+  }
+
+  test("parseRow(\".......321\") returns the correct value") {
+    assertEquals(
+      parseRow(".......321"),
+      Some(List(Annotated(PartNumber(321), pos0Based = 7, length = 3)))
+    )
+  }
+
+  test("parse(bigInput) is defined") {
+    assert(parse(bigInput).isDefined)
+  }
+
+  test("getParts(smallRows) returns the correct Parts") {
+    assertEquals(
+      getParts(smallRows),
+      List(
+        Part(Symbol('*'), PartNumber(467)),
+        Part(Symbol('*'), PartNumber(35)),
+        Part(Symbol('#'), PartNumber(633)),
+        Part(Symbol('*'), PartNumber(617)),
+        Part(Symbol('+'), PartNumber(592)),
+        Part(Symbol('*'), PartNumber(755)),
+        Part(Symbol('$'), PartNumber(664)),
+        Part(Symbol('*'), PartNumber(598))
+      )
+    )
+  }
+
+  test("getPartNumbersSum(smallInput) returns Some(4361)") {
+    assertEquals(getPartNumbersSum(smallInput), Some(4361))
+  }
+
+  test("getPartNumbersSum(bigInput) returns Some(554_003)") {
+    assertEquals(getPartNumbersSum(bigInput), Some(554_003))
+  }
+
+object Day3Suite:
+
+  val smallInput: List[String] = List(
+    "467..114..",
+    "...*......",
+    "..35..633.",
+    "......#...",
+    "617*......",
+    ".....+.58.",
+    "..592.....",
+    "......755.",
+    "...$.*....",
+    ".664.598.."
+  )
+  val smallRows: List[List[Annotated[Literal]]] = List(
+    List(
+      Annotated(PartNumber(467), pos0Based = 0, length = 3),
+      Annotated(PartNumber(114), pos0Based = 5, length = 3)
+    ),
+    List(Annotated(Symbol('*'), pos0Based = 3, length = 1)),
+    List(
+      Annotated(PartNumber(35), pos0Based = 2, length = 2),
+      Annotated(PartNumber(633), pos0Based = 6, length = 3)
+    ),
+    List(Annotated(Symbol('#'), pos0Based = 6, length = 1)),
+    List(
+      Annotated(PartNumber(617), pos0Based = 0, length = 3),
+      Annotated(Symbol('*'), pos0Based = 3, length = 1)
+    ),
+    List(
+      Annotated(Symbol('+'), pos0Based = 5, length = 1),
+      Annotated(PartNumber(58), pos0Based = 7, length = 2)
+    ),
+    List(Annotated(PartNumber(592), pos0Based = 2, length = 3)),
+    List(Annotated(PartNumber(755), pos0Based = 6, length = 3)),
+    List(
+      Annotated(Symbol('$'), pos0Based = 3, length = 1),
+      Annotated(Symbol('*'), pos0Based = 5, length = 1)
+    ),
+    List(
+      Annotated(PartNumber(664), pos0Based = 1, length = 3),
+      Annotated(PartNumber(598), pos0Based = 5, length = 3)
+    )
+  )
+
+  val bigInput: List[String] = getLinesFromFile("src/test/scala/day3_input.txt")
