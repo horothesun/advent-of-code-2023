49bb464181d72c8b06ef2dcc1d90dce78a202199
horothesun <horothesun@gmail.com>
2024-03-23 11:02:56 +0000

    Day 6: input parsing

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -1,4 +1,24 @@
-import cats.derived.*
 import cats.implicits.*
 
-def day6: Int = 42
+case class Time(millis: Int)
+case class Distance(millimeters: Int)
+
+case class Race(allowance: Time, best: Distance)
+
+def parseRaces(input: List[String]): Option[List[Race]] = input match
+  case ts :: ds :: Nil =>
+    (parseTimes(ts), parseDistances(ds)).flatMapN((times, distances) =>
+      if (times.length != distances.length) None else Some(times.zip(distances).map(Race.apply))
+    )
+  case _ => None
+
+def parseAs[T](apply: Int => T, s: String): Option[List[T]] =
+  s.split(' ').toList.filterNot(_.isEmpty).traverse(_.toIntOption.map(apply))
+
+def parseTimes(times: String): Option[List[Time]] = times.toList match
+  case 'T' :: 'i' :: 'm' :: 'e' :: ':' :: ts => parseAs(Time.apply, ts.mkString)
+  case _                                     => None
+
+def parseDistances(distances: String): Option[List[Distance]] = distances.toList match
+  case 'D' :: 'i' :: 's' :: 't' :: 'a' :: 'n' :: 'c' :: 'e' :: ':' :: ds => parseAs(Distance.apply, ds.mkString)
+  case _                                                                 => None
--- src/test/scala/Day6Suite.scala
+++ src/test/scala/Day6Suite.scala
@@ -1,12 +1,48 @@
 import munit.ScalaCheckSuite
-import org.scalacheck.Gen
-import org.scalacheck.Prop.*
 import Day6Suite.*
 
 class Day6Suite extends ScalaCheckSuite:
 
-  test("day6 == 42") {
-    assertEquals(day6, 42)
+  test("parseAt[Int](identity, \"      7  15   30\")") {
+    assertEquals(parseAs[Int](identity, "      7  15   30"), Some(List(7, 15, 30)))
+  }
+
+  test("parseTimes(\"Time:      7  15   30\")") {
+    assertEquals(parseTimes("Time:      7  15   30"), Some(List(Time(7), Time(15), Time(30))))
+  }
+
+  test("parseDistances(\"Distance:  9  40  200\")") {
+    assertEquals(
+      parseDistances("Distance:  9  40  200"),
+      Some(List(Distance(9), Distance(40), Distance(200)))
+    )
+  }
+
+  test("smallInput parses correctly") {
+    assertEquals(
+      parseRaces(smallInput),
+      Some(
+        List(
+          Race(allowance = Time(7), best = Distance(9)),
+          Race(allowance = Time(15), best = Distance(40)),
+          Race(allowance = Time(30), best = Distance(200))
+        )
+      )
+    )
+  }
+
+  test("bigInput parses correctly") {
+    assertEquals(
+      parseRaces(bigInput),
+      Some(
+        List(
+          Race(allowance = Time(61), best = Distance(643)),
+          Race(allowance = Time(70), best = Distance(1184)),
+          Race(allowance = Time(90), best = Distance(1362)),
+          Race(allowance = Time(66), best = Distance(1041))
+        )
+      )
+    )
   }
 
 object Day6Suite:
