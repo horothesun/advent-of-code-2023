27cab2bd09238ac52b498f87687eec9e6a7f8736
horothesun <horothesun@gmail.com>
2024-04-08 23:44:06 +0100

    Day 9 part 1

---
+++ src/main/scala/Day9.scala
@@ -0,0 +1,39 @@
+import cats.data.NonEmptyList
+import cats.implicits.*
+
+object Day9:
+
+  case class History(values: NonEmptyList[Long])
+  object History:
+    def parse(s: String): Option[History] = s.split(' ').toList match
+      case Nil    => None
+      case vs @ _ => vs.traverse(_.toLongOption).flatMap(_.toNel.map(History.apply))
+
+  def parse(inputs: NonEmptyList[String]): Option[NonEmptyList[History]] = inputs.traverse(History.parse)
+
+  def allDifferences(h: History): List[NonEmptyList[Long]] = allReversedDifferences(h).map(_.reverse)
+
+  def allReversedDifferences(h: History): List[NonEmptyList[Long]] =
+    List.unfold[NonEmptyList[Long], NonEmptyList[Long]](init = h.values.reverse) { reversed =>
+      reversedDifferences(reversed).flatMap(diffs => if (reversed.forall(_ == 0)) None else Some((diffs, diffs)))
+    }
+
+  def differences(vs: NonEmptyList[Long]): Option[NonEmptyList[Long]] = reversedDifferences(vs.reverse).map(_.reverse)
+
+  def reversedDifferences(reversed: NonEmptyList[Long]): Option[NonEmptyList[Long]] =
+    reversed.toList.zip(reversed.tail).map((v1, v2) => v1 - v2).toNel
+
+  def allPredictions(h: History): List[Long] =
+    val triangle = NonEmptyList(h.values.reverse, allReversedDifferences(h)).reverse
+    val firstPred = if (triangle.head.forall(_ == 0)) 0 else triangle.head.head
+    triangle
+      .foldLeft[(List[Long], Long)]((List.empty, firstPred)) { case ((preds, lastPred), revDiffs) =>
+        val newPred = revDiffs.head + lastPred
+        (newPred :: preds, newPred)
+      }
+      ._1
+
+  def prediction(h: History): Option[Long] = allPredictions(h).headOption
+
+  def allPredictionsSum(inputs: NonEmptyList[String]): Option[Long] =
+    parse(inputs).flatMap(hs => hs.traverse(prediction)).map(_.sumAll)
---
+++ src/test/scala/Day9Suite.scala
@@ -0,0 +1,93 @@
+import cats.data.NonEmptyList
+import cats.implicits.*
+import munit.ScalaCheckSuite
+import Day9.*
+import Day9Suite.*
+
+class Day9Suite extends ScalaCheckSuite:
+
+  test("\"0 3 6 9 12 15\" differences are \"3 3 3 3 3\"") {
+    assertEquals(
+      differences(NonEmptyList.of(0, 3, 6, 9, 12, 15)),
+      Some(NonEmptyList.of[Long](3, 3, 3, 3, 3))
+    )
+  }
+
+  test("\"1 3 6 10 15 21\" differences are \"2 3 4 5 6\"") {
+    assertEquals(
+      differences(NonEmptyList.of(1, 3, 6, 10, 15, 21)),
+      Some(NonEmptyList.of[Long](2, 3, 4, 5, 6))
+    )
+  }
+
+  test("\"10 13 16 21 30 45\" differences are \"3 3 5 9 15\"") {
+    assertEquals(
+      differences(NonEmptyList.of(10, 13, 16, 21, 30, 45)),
+      Some(NonEmptyList.of[Long](3, 3, 5, 9, 15))
+    )
+  }
+
+  test("\"0 3 6 9 12 15\" all differences are \"3 3 3 3 3\", \"0 0 0 0\"") {
+    assertEquals(
+      allDifferences(History(NonEmptyList.of(0, 3, 6, 9, 12, 15))),
+      List(
+        NonEmptyList.of[Long](3, 3, 3, 3, 3),
+        NonEmptyList.of[Long](0, 0, 0, 0)
+      )
+    )
+  }
+
+  test("\"1 3 6 10 15 21\" all differences are \"2 3 4 5 6\", \"1 1 1 1\", \"0 0 0\"") {
+    assertEquals(
+      allDifferences(History(NonEmptyList.of(1, 3, 6, 10, 15, 21))),
+      List(
+        NonEmptyList.of[Long](2, 3, 4, 5, 6),
+        NonEmptyList.of[Long](1, 1, 1, 1),
+        NonEmptyList.of[Long](0, 0, 0)
+      )
+    )
+  }
+
+  test("\"10 13 16 21 30 45\" all differences are \"3 3 5 9 15\", \"0 2 4 6\", \"2 2 2\", \"0 0\"") {
+    assertEquals(
+      allDifferences(History(NonEmptyList.of(10, 13, 16, 21, 30, 45))),
+      List(
+        NonEmptyList.of[Long](3, 3, 5, 9, 15),
+        NonEmptyList.of[Long](0, 2, 4, 6),
+        NonEmptyList.of[Long](2, 2, 2),
+        NonEmptyList.of[Long](0, 0)
+      )
+    )
+  }
+
+  test("\"0 3 6 9 12 15\" all predictions are List(18, 3, 0)") {
+    assertEquals(allPredictions(History(NonEmptyList.of(0, 3, 6, 9, 12, 15))), List(18L, 3L, 0L))
+  }
+
+  test("\"1 3 6 10 15 21\" all predictions are List(28, 7, 1, 0)") {
+    assertEquals(allPredictions(History(NonEmptyList.of(1, 3, 6, 10, 15, 21))), List(28L, 7L, 1L, 0L))
+  }
+
+  test("\"10 13 16 21 30 45\" all predictions are List(68, 23, 8, 2, 0)") {
+    assertEquals(
+      allPredictions(History(NonEmptyList.of(10, 13, 16, 21, 30, 45))),
+      List(68L, 23L, 8L, 2L, 0L)
+    )
+  }
+
+  test("small input all prediction sum is 114") {
+    val smallInput = NonEmptyList.of(
+      "0 3 6 9 12 15",
+      "1 3 6 10 15 21",
+      "10 13 16 21 30 45"
+    )
+    assertEquals(allPredictionsSum(smallInput), Some(114L))
+  }
+
+  test("big input all prediction sum is 1_702_218_515") {
+    assertEquals(allPredictionsSum(bigInput), Some(1_702_218_515L))
+  }
+
+object Day9Suite:
+
+  val bigInput: NonEmptyList[String] = getLinesFromFile("src/test/scala/day9_input.txt").toNel.get
