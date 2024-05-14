93875d0f27eb7a77664eb86fc70747779b7cbeca
horothesun <horothesun@gmail.com>
2024-05-15 00:04:57 +0100

    Day 10 part 1: WIP

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -1,34 +1,101 @@
-import Day10.Pipe.*
 import cats.implicits.*
+import Day10.PipeType.*
+import Day10.Tile.*
 
 object Day10:
 
-  def day10: Int = 42
+  enum PipeType:
+    case Vertical, Horizontal, NorthAndWest, NorthAndEast, SouthAndWest, SouthAndEast
 
-  case object StartPipe
-  enum Pipe:
-    case Horizontal, Vertical, LeftAndTop, LeftAndBottom, RightAndTop, RightAndBottom
+  object PipeType:
+    def from(c: Char): Option[PipeType] = c match
+      case '|' => Some(Vertical)
+      case '-' => Some(Horizontal)
+      case 'J' => Some(NorthAndWest)
+      case 'L' => Some(NorthAndEast)
+      case '7' => Some(SouthAndWest)
+      case 'F' => Some(SouthAndEast)
+      case _   => None
+
+  case class CandidateConnections(north: Option[Pos], south: Option[Pos], west: Option[Pos], east: Option[Pos])
+  object CandidateConnections:
+    def from(pipeType: PipeType, pos: Pos): CandidateConnections = pipeType match
+      case Vertical     => CandidateConnections(Some(pos.north), Some(pos.south), west = None, east = None)
+      case Horizontal   => CandidateConnections(north = None, south = None, Some(pos.west), Some(pos.east))
+      case NorthAndWest => CandidateConnections(Some(pos.north), south = None, Some(pos.west), east = None)
+      case NorthAndEast => CandidateConnections(Some(pos.north), south = None, west = None, Some(pos.east))
+      case SouthAndWest => CandidateConnections(north = None, Some(pos.south), Some(pos.west), east = None)
+      case SouthAndEast => CandidateConnections(north = None, Some(pos.south), west = None, Some(pos.east))
 
   case class Pos(row: Int, col: Int):
-    def left: Pos = Pos(row, col - 1)
-    def top: Pos = Pos(row + 1, col)
-    def right: Pos = Pos(row, col + 1)
-    def bottom: Pos = Pos(row - 1, col)
+    lazy val north: Pos = Pos(row + 1, col)
+    lazy val south: Pos = Pos(row - 1, col)
+    lazy val west: Pos = Pos(row, col - 1)
+    lazy val east: Pos = Pos(row, col + 1)
 
-  case class Tile(pipe: Pipe, pos: Pos):
-    def candidateConnections: (Pos, Pos) = pipe match
-      case Horizontal     => (pos.left, pos.right)
-      case Vertical       => (pos.top, pos.bottom)
-      case LeftAndTop     => (pos.left, pos.top)
-      case LeftAndBottom  => (pos.left, pos.bottom)
-      case RightAndTop    => (pos.right, pos.top)
-      case RightAndBottom => (pos.right, pos.bottom)
+  enum Tile:
+    case Ground, Start
+    case Pipe(pipeType: PipeType)
 
-  enum FieldItem:
-    case Start
-    case PipeItem(pipe: Pipe)
+  object Tile:
+    def from(c: Char): Option[Tile] = PipeType.from(c).map(Pipe.apply).orElse {
+      c match
+        case '.' => Some(Ground)
+        case 'S' => Some(Start)
+        case _   => None
+    }
 
-  case class Field(rows: Vector[Vector[FieldItem]]):
-    def get(pos: Pos): Option[FieldItem] = rows.get(pos.row).flatMap(_.get(pos.col))
+  case class Field(rows: Vector[Vector[Tile]]):
+    def get(pos: Pos): Option[Tile] = rows.get(pos.row).flatMap(_.get(pos.col))
 
-    def allConnectedTiles(pos: Pos): List[Tile] = ???
+    def oneStepFrom(pos: Pos): Set[(Tile, Pos)] = get(pos).toSet.flatMap {
+      case Ground         => Set.empty
+      case Start          => oneStepFromStart(pos)
+      case Pipe(pipeType) => oneStepFrom(pipeType, pos)
+    }
+
+    // input should guarantee the output to have exactly 2 elements
+    def oneStepFromStart(pos: Pos): Set[(Tile, Pos)] =
+      List(
+        oneStepNorthOfStart(pos),
+        oneStepSouthOfStart(pos),
+        oneStepWestOfStart(pos),
+        oneStepEastOfStart(pos)
+      ).mapFilter(identity).toSet
+
+    def oneStepNorthOfStart(pos: Pos): Option[(Tile, Pos)] = get(pos.north).collect {
+      case t @ Pipe(Vertical)     => t
+      case t @ Pipe(SouthAndWest) => t
+      case t @ Pipe(SouthAndEast) => t
+    }.map((_, pos.north))
+    def oneStepSouthOfStart(pos: Pos): Option[(Tile, Pos)] = get(pos.south).collect {
+      case t @ Pipe(Vertical)     => t
+      case t @ Pipe(NorthAndWest) => t
+      case t @ Pipe(NorthAndEast) => t
+    }.map((_, pos.south))
+    def oneStepWestOfStart(pos: Pos): Option[(Tile, Pos)] = get(pos.west).collect {
+      case t @ Pipe(Horizontal)   => t
+      case t @ Pipe(NorthAndEast) => t
+      case t @ Pipe(SouthAndEast) => t
+    }.map((_, pos.west))
+    def oneStepEastOfStart(pos: Pos): Option[(Tile, Pos)] = get(pos.east).collect {
+      case t @ Pipe(Horizontal)   => t
+      case t @ Pipe(NorthAndWest) => t
+      case t @ Pipe(SouthAndWest) => t
+    }.map((_, pos.east))
+
+    def oneStepFrom(pipeType: PipeType, pos: Pos): Set[(Tile, Pos)] =
+      def validTiles(optPs: Option[Pos]*): Set[(Tile, Pos)] =
+        optPs.toList.mapFilter(optP => optP.flatMap(p => get(p).map((_, p)))).toSet
+      val cc = CandidateConnections.from(pipeType, pos)
+      pipeType match
+        case Vertical     => validTiles(cc.north, cc.south)
+        case Horizontal   => validTiles(cc.west, cc.east)
+        case NorthAndWest => validTiles(cc.north, cc.west)
+        case NorthAndEast => validTiles(cc.north, cc.east)
+        case SouthAndWest => validTiles(cc.south, cc.west)
+        case SouthAndEast => validTiles(cc.south, cc.east)
+
+  object Field:
+    def from(input: List[String]): Option[Field] =
+      input.traverse(r => r.toList.traverse(Tile.from).map(_.toVector)).map(rs => Field(rs.toVector))
--- src/test/scala/Day10Suite.scala
+++ src/test/scala/Day10Suite.scala
@@ -1,15 +1,67 @@
 import munit.ScalaCheckSuite
-import org.scalacheck.Gen
-import org.scalacheck.Prop.*
 import Day10.*
+import Day10.PipeType.*
+import Day10.Tile.*
 import Day10Suite.*
 
 class Day10Suite extends ScalaCheckSuite:
 
-  test("day10 == 42") {
-    assertEquals(day10, 42)
+  test("parsing small input 1") {
+    val expected = Field(rows =
+      Vector(
+        Vector(Horizontal, NorthAndEast, Vertical, SouthAndEast, SouthAndWest).map(Pipe.apply),
+        Vector(Pipe(SouthAndWest), Start, Pipe(Horizontal), Pipe(SouthAndWest), Pipe(Vertical)),
+        Vector(NorthAndEast, Vertical, SouthAndWest, Vertical, Vertical).map(Pipe.apply),
+        Vector(Horizontal, NorthAndEast, Horizontal, NorthAndWest, Vertical).map(Pipe.apply),
+        Vector(NorthAndEast, Vertical, Horizontal, NorthAndWest, SouthAndEast).map(Pipe.apply)
+      )
+    )
+    assertEquals(Field.from(smallInput1), Some(expected))
+  }
+
+  test("parsing small simplified input 1") {
+    val expected = Field(rows =
+      Vector(
+        Vector.fill(5)(Ground),
+        Vector(Ground, Start, Pipe(Horizontal), Pipe(SouthAndWest), Ground),
+        Vector(Ground, Pipe(Vertical), Ground, Pipe(Vertical), Ground),
+        Vector(Ground, Pipe(NorthAndEast), Pipe(Horizontal), Pipe(NorthAndWest), Ground),
+        Vector.fill(5)(Ground)
+      )
+    )
+    assertEquals(Field.from(smallSimplifiedInput1), Some(expected))
   }
 
 object Day10Suite:
 
-  val bigInput: List[String] = getLinesFromFile("src/test/scala/day10_input.txt")
+//  val bigInput: List[String] = getLinesFromFile("src/test/scala/day10_input.txt")
+
+  val smallInput1: List[String] = List(
+    "-L|F7",
+    "7S-7|",
+    "L|7||",
+    "-L-J|",
+    "L|-JF"
+  )
+  val smallSimplifiedInput1: List[String] = List(
+    ".....",
+    ".S-7.",
+    ".|.|.",
+    ".L-J.",
+    "....."
+  )
+
+  val smallInput2: List[String] = List(
+    "7-F7-",
+    ".FJ|7",
+    "SJLL7",
+    "|F--J",
+    "LJ.LJ"
+  )
+  val smallSimplifiedInput2: List[String] = List(
+    "..F7.",
+    ".FJ|.",
+    "SJ.L7",
+    "|F--J",
+    "LJ..."
+  )
