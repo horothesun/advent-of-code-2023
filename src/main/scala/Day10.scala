e72b0b592071167b450c9b9ee5a0a370a6bd68d5
horothesun <horothesun@gmail.com>
2024-05-15 21:44:39 +0100

    Day 10 part 1

--- src/main/scala/Day10.scala
+++ src/main/scala/Day10.scala
@@ -1,4 +1,6 @@
+import cats.data.NonEmptyList
 import cats.implicits.*
+import scala.annotation.tailrec
 import Day10.PipeType.*
 import Day10.Tile.*
 
@@ -8,7 +10,7 @@ object Day10:
     case Vertical, Horizontal, NorthAndWest, NorthAndEast, SouthAndWest, SouthAndEast
 
   object PipeType:
-    def from(c: Char): Option[PipeType] = c match
+    def parse(c: Char): Option[PipeType] = c match
       case '|' => Some(Vertical)
       case '-' => Some(Horizontal)
       case 'J' => Some(NorthAndWest)
@@ -19,7 +21,7 @@ object Day10:
 
   case class CandidateConnections(north: Option[Pos], south: Option[Pos], west: Option[Pos], east: Option[Pos])
   object CandidateConnections:
-    def from(pipeType: PipeType, pos: Pos): CandidateConnections = pipeType match
+    def parse(pipeType: PipeType, pos: Pos): CandidateConnections = pipeType match
       case Vertical     => CandidateConnections(Some(pos.north), Some(pos.south), west = None, east = None)
       case Horizontal   => CandidateConnections(north = None, south = None, Some(pos.west), Some(pos.east))
       case NorthAndWest => CandidateConnections(Some(pos.north), south = None, Some(pos.west), east = None)
@@ -28,8 +30,8 @@ object Day10:
       case SouthAndEast => CandidateConnections(north = None, Some(pos.south), west = None, Some(pos.east))
 
   case class Pos(row: Int, col: Int):
-    lazy val north: Pos = Pos(row + 1, col)
-    lazy val south: Pos = Pos(row - 1, col)
+    lazy val north: Pos = Pos(row - 1, col)
+    lazy val south: Pos = Pos(row + 1, col)
     lazy val west: Pos = Pos(row, col - 1)
     lazy val east: Pos = Pos(row, col + 1)
 
@@ -38,14 +40,25 @@ object Day10:
     case Pipe(pipeType: PipeType)
 
   object Tile:
-    def from(c: Char): Option[Tile] = PipeType.from(c).map(Pipe.apply).orElse {
+    def parse(c: Char): Option[Tile] = PipeType.parse(c).map(Pipe.apply).orElse {
       c match
         case '.' => Some(Ground)
         case 'S' => Some(Start)
         case _   => None
     }
 
+  case class Loop(firstPath: NonEmptyList[Pos], secondPath: NonEmptyList[Pos])
+
   case class Field(rows: Vector[Vector[Tile]]):
+    def getStartPos: Option[Pos] =
+      val rowIndexes = Range(start = 0, end = rows.length).toList
+      val colIndexes = rows.get(0).fold(ifEmpty = List.empty[Int])(r => Range(start = 0, end = r.length).toList)
+      val allPositions = rowIndexes.flatMap(row => colIndexes.map(col => Pos(row, col)))
+      allPositions.collectFirst { p =>
+        get(p) match
+          case Some(Start) => p
+      }
+
     def get(pos: Pos): Option[Tile] = rows.get(pos.row).flatMap(_.get(pos.col))
 
     def oneStepFrom(pos: Pos): Set[(Tile, Pos)] = get(pos).toSet.flatMap {
@@ -54,7 +67,6 @@ object Day10:
       case Pipe(pipeType) => oneStepFrom(pipeType, pos)
     }
 
-    // input should guarantee the output to have exactly 2 elements
     def oneStepFromStart(pos: Pos): Set[(Tile, Pos)] =
       List(
         oneStepNorthOfStart(pos),
@@ -87,7 +99,7 @@ object Day10:
     def oneStepFrom(pipeType: PipeType, pos: Pos): Set[(Tile, Pos)] =
       def validTiles(optPs: Option[Pos]*): Set[(Tile, Pos)] =
         optPs.toList.mapFilter(optP => optP.flatMap(p => get(p).map((_, p)))).toSet
-      val cc = CandidateConnections.from(pipeType, pos)
+      val cc = CandidateConnections.parse(pipeType, pos)
       pipeType match
         case Vertical     => validTiles(cc.north, cc.south)
         case Horizontal   => validTiles(cc.west, cc.east)
@@ -96,6 +108,43 @@ object Day10:
         case SouthAndWest => validTiles(cc.south, cc.west)
         case SouthAndEast => validTiles(cc.south, cc.east)
 
+    def getLoop: Option[Loop] = for {
+      s <- getStartPos
+      case (fst, snd) <- exactly2Adjacent(s)
+      loop <- getLoopPaths(fstLast = fst, fstPath = NonEmptyList.one(s), sndLast = snd, sndPath = NonEmptyList.one(s))
+    } yield loop
+
+    @tailrec
+    final def getLoopPaths(
+      fstLast: Pos,
+      fstPath: NonEmptyList[Pos],
+      sndLast: Pos,
+      sndPath: NonEmptyList[Pos]
+    ): Option[Loop] =
+      if (fstLast == sndLast) Some(Loop(fstPath :+ fstLast, sndPath :+ sndLast))
+      else
+        (
+          nextUnvisited(fstLast, fstPath),
+          nextUnvisited(sndLast, sndPath)
+        ).tupled match
+          case Some((newFstLast, newSndLast)) =>
+            getLoopPaths(newFstLast, fstPath :+ fstLast, newSndLast, sndPath :+ sndLast)
+          case None => None
+
+    def exactly2Adjacent(pos: Pos): Option[(Pos, Pos)] = oneStepFrom(pos).toList match
+      case (_, fst) :: (_, snd) :: Nil => Some((fst, snd))
+      case _                           => None
+
+    def nextUnvisited(pos: Pos, visited: NonEmptyList[Pos]): Option[Pos] = exactly2Adjacent(pos).flatMap {
+      case (fst, snd) =>
+        List(fst, snd).filterNot(visited.toList.contains) match
+          case p :: Nil => Some(p)
+          case _        => None
+    }
+
   object Field:
-    def from(input: List[String]): Option[Field] =
-      input.traverse(r => r.toList.traverse(Tile.from).map(_.toVector)).map(rs => Field(rs.toVector))
+    def parse(input: List[String]): Option[Field] =
+      input.traverse(r => r.toList.traverse(Tile.parse).map(_.toVector)).map(rs => Field(rs.toVector))
+
+  def stepsCountToFarthestInLoop(input: List[String]): Option[Int] =
+    Field.parse(input).flatMap(f => f.getLoop.map(_.firstPath.length - 1))
--- src/test/scala/Day10Suite.scala
+++ src/test/scala/Day10Suite.scala
@@ -1,3 +1,4 @@
+import cats.data.NonEmptyList
 import munit.ScalaCheckSuite
 import Day10.*
 import Day10.PipeType.*
@@ -16,7 +17,7 @@ class Day10Suite extends ScalaCheckSuite:
         Vector(NorthAndEast, Vertical, Horizontal, NorthAndWest, SouthAndEast).map(Pipe.apply)
       )
     )
-    assertEquals(Field.from(smallInput1), Some(expected))
+    assertEquals(Field.parse(smallInput1), Some(expected))
   }
 
   test("parsing small simplified input 1") {
@@ -29,12 +30,163 @@ class Day10Suite extends ScalaCheckSuite:
         Vector.fill(5)(Ground)
       )
     )
-    assertEquals(Field.from(smallSimplifiedInput1), Some(expected))
+    assertEquals(Field.parse(smallSimplifiedInput1), Some(expected))
+  }
+
+  test("start position for small input 1 is Pos(row = 1, col = 1)") {
+    assertEquals(Field.parse(smallInput1).flatMap(_.getStartPos), Some(Pos(row = 1, col = 1)))
+  }
+
+  test("start position for small simplified input 1 is Pos(row = 1, col = 1)") {
+    assertEquals(
+      Field.parse(smallSimplifiedInput1).flatMap(_.getStartPos),
+      Some(Pos(row = 1, col = 1))
+    )
+  }
+
+  test("start position for small input 2 is Pos(row = 2, col = 0)") {
+    assertEquals(Field.parse(smallInput2).flatMap(_.getStartPos), Some(Pos(row = 2, col = 0)))
+  }
+
+  test("start position for small simplified input 2 is Pos(row = 2, col = 0)") {
+    assertEquals(
+      Field.parse(smallSimplifiedInput2).flatMap(_.getStartPos),
+      Some(Pos(row = 2, col = 0))
+    )
+  }
+
+  test("start position for big input is defined") {
+    assert(Field.parse(bigInput).flatMap(_.getStartPos).isDefined)
+  }
+
+  test("one-step moves from start found for small input 1") {
+    assertEquals(
+      for {
+        field <- Field.parse(smallInput1)
+        start <- field.getStartPos
+      } yield field.oneStepFrom(start),
+      Some(Set((Pipe(Horizontal), Pos(row = 1, col = 2)), (Pipe(Vertical), Pos(row = 2, col = 1))))
+    )
+  }
+
+  test("one-step moves from start found for small simplified input 1") {
+    assertEquals(
+      for {
+        field <- Field.parse(smallSimplifiedInput1)
+        start <- field.getStartPos
+      } yield field.oneStepFrom(start),
+      Some(Set((Pipe(Horizontal), Pos(row = 1, col = 2)), (Pipe(Vertical), Pos(row = 2, col = 1))))
+    )
+  }
+
+  test("one-step moves from start found for small input 2") {
+    assertEquals(
+      for {
+        field <- Field.parse(smallInput2)
+        start <- field.getStartPos
+      } yield field.oneStepFrom(start),
+      Some(Set((Pipe(NorthAndWest), Pos(row = 2, col = 1)), (Pipe(Vertical), Pos(row = 3, col = 0))))
+    )
+  }
+
+  test("one-step moves from start found for small simplified input 2") {
+    assertEquals(
+      for {
+        field <- Field.parse(smallSimplifiedInput2)
+        start <- field.getStartPos
+      } yield field.oneStepFrom(start),
+      Some(Set((Pipe(NorthAndWest), Pos(row = 2, col = 1)), (Pipe(Vertical), Pos(row = 3, col = 0))))
+    )
+  }
+
+  test("#2 one-step moves from start found for big input") {
+    assertEquals(
+      for {
+        field <- Field.parse(bigInput)
+        start <- field.getStartPos
+      } yield field.oneStepFrom(start).size,
+      Some(2)
+    )
+  }
+
+  test("loop for small input 1") {
+    assertEquals(
+      Field.parse(smallInput1).flatMap(_.getLoop),
+      Some(
+        Loop(
+          firstPath = NonEmptyList.of(Pos(1, 1), Pos(2, 1), Pos(3, 1), Pos(3, 2), Pos(3, 3)),
+          secondPath = NonEmptyList.of(Pos(1, 1), Pos(1, 2), Pos(1, 3), Pos(2, 3), Pos(3, 3))
+        )
+      )
+    )
+  }
+
+  test("loop for small input 1 is same of small simplified input 1") {
+    assertEquals(
+      Field.parse(smallInput1).flatMap(_.getLoop),
+      Field.parse(smallSimplifiedInput1).flatMap(_.getLoop)
+    )
+  }
+
+  test("loop for small input 2") {
+    assertEquals(
+      Field.parse(smallInput2).flatMap(_.getLoop),
+      Some(
+        Loop(
+          firstPath = NonEmptyList
+            .of((2, 0), (3, 0), (4, 0), (4, 1), (3, 1), (3, 2), (3, 3), (3, 4), (2, 4))
+            .map(Pos.apply.tupled),
+          secondPath = NonEmptyList
+            .of((2, 0), (2, 1), (1, 1), (1, 2), (0, 2), (0, 3), (1, 3), (2, 3), (2, 4))
+            .map(Pos.apply.tupled)
+        )
+      )
+    )
+  }
+
+  test("loop for small input 2 is same of small simplified input 2") {
+    assertEquals(
+      Field.parse(smallInput2).flatMap(_.getLoop),
+      Field.parse(smallSimplifiedInput2).flatMap(_.getLoop)
+    )
+  }
+
+  test("steps count to farthest point in loop is 4 for small input 1") {
+    assertEquals(stepsCountToFarthestInLoop(smallInput1), Some(4))
+  }
+
+  test("steps count to farthest point in loop is same for small input 1 and simplified one") {
+    assertEquals(
+      stepsCountToFarthestInLoop(smallInput1),
+      stepsCountToFarthestInLoop(smallSimplifiedInput1)
+    )
+  }
+
+  test("steps count to farthest point in loop is 8 for small input 2") {
+    assertEquals(stepsCountToFarthestInLoop(smallInput2), Some(8))
+  }
+
+  test("steps count to farthest point in loop is same for small input 2 and simplified one") {
+    assertEquals(
+      stepsCountToFarthestInLoop(smallInput2),
+      stepsCountToFarthestInLoop(smallSimplifiedInput2)
+    )
+  }
+
+  test("loop branches has same length for big input") {
+    assertEquals(
+      Field.parse(bigInput).flatMap(_.getLoop.map(l => l.firstPath.length == l.secondPath.length)),
+      Some(true)
+    )
+  }
+
+  test("steps count to farthest point in loop is 6_828 for big input") {
+    assertEquals(stepsCountToFarthestInLoop(bigInput), Some(6_828))
   }
 
 object Day10Suite:
 
-//  val bigInput: List[String] = getLinesFromFile("src/test/scala/day10_input.txt")
+  val bigInput: List[String] = getLinesFromFile("src/test/scala/day10_input.txt")
 
   val smallInput1: List[String] = List(
     "-L|F7",
