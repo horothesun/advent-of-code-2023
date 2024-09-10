d3cf73679e4774364f3dbdfc77103051f07f4a37
horothesun <horothesun@gmail.com>
2024-09-10 14:52:39 +0100

    Day 11 part 1: WIP

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -1,6 +1,77 @@
+import cats.*
+import cats.data.NonEmptyList
 import cats.derived.*
 import cats.syntax.all.*
+import scala.annotation.tailrec
+import Day11.Image.*
+import Day11.NonEmptyMatrix.*
+import Day11.Pixel.*
 
 object Day11:
 
-  def day11: Int = 42
+  case class Pos(row: Int, col: Int)
+
+  case class NonEmptyMatrix[A](rows: NonEmptyList[NonEmptyList[A]]) derives Functor, Foldable:
+    lazy val rotatedCW: NonEmptyMatrix[A] = transposed.vFlipped
+    lazy val rotatedCCW: NonEmptyMatrix[A] = transposed.hFlipped
+    lazy val transposed: NonEmptyMatrix[A] = NonEmptyMatrix(transpose(rows))
+    lazy val vFlipped: NonEmptyMatrix[A] = NonEmptyMatrix(rows.map(_.reverse))
+    lazy val hFlipped: NonEmptyMatrix[A] = NonEmptyMatrix(rows.reverse)
+
+    lazy val zipWithPos: NonEmptyMatrix[(A, Pos)] = NonEmptyMatrix(
+      rows.zipWithIndex.map((r, rowId) => r.zipWithIndex.map((a, colId) => (a, Pos(rowId, colId))))
+    )
+
+    def collect[B](pf: PartialFunction[A, B]): List[B] = this.toList.collect(pf)
+
+  object NonEmptyMatrix:
+    def transpose[A](rows: NonEmptyList[NonEmptyList[A]]): NonEmptyList[NonEmptyList[A]] =
+      def heads(rows: NonEmptyList[NonEmptyList[A]]): NonEmptyList[A] = rows.map(_.head)
+      def tails(rows: NonEmptyList[NonEmptyList[A]]): List[List[A]] = rows.toList.map(_.tail)
+      @tailrec
+      def aux(acc: NonEmptyList[NonEmptyList[A]], rows: List[List[A]]): NonEmptyList[NonEmptyList[A]] =
+        rows match
+          case Nil      => acc
+          case Nil :: _ => acc
+          case (_ :: _) :: _ =>
+            val (newAcc, newRows) =
+              rows
+                .traverse(_.toNel)
+                .flatMap(_.toNel)
+                .map(rs => (acc :+ heads(rs), tails(rs)))
+                .getOrElse((acc, List.empty))
+            aux(newAcc, newRows)
+      aux(acc = NonEmptyList.of(heads(rows)), tails(rows))
+
+  enum Pixel:
+    case EmptySpace, Galaxy
+  object Pixel:
+    def parse(c: Char): Option[Pixel] =
+      c match
+        case '.' => Some(EmptySpace)
+        case '#' => Some(Galaxy)
+        case _   => None
+
+  case class Image(pixels: NonEmptyMatrix[Pixel]):
+
+    def expanded: Image =
+      val expandedRows = duplicateAllEmptySpaceRows(pixels)
+      val expandedRowsAndColumns = duplicateAllEmptySpaceRows(expandedRows.rotatedCW).rotatedCCW
+      Image(expandedRowsAndColumns)
+
+  object Image:
+
+    def apply(rows: NonEmptyList[NonEmptyList[Pixel]]): Image = Image(pixels = NonEmptyMatrix(rows))
+
+    def apply(head: NonEmptyList[Pixel], tail: NonEmptyList[Pixel]*): Image =
+      Image.apply(rows = NonEmptyList(head, tail.toList))
+
+    def parse(input: List[String]): Option[Image] =
+      for {
+        nelInput <- input.toNel
+        rows <- nelInput.traverse(_.toList.toNel.flatMap(_.traverse(Pixel.parse)))
+      } yield Image(rows)
+
+    def duplicateAllEmptySpaceRows(ps: NonEmptyMatrix[Pixel]): NonEmptyMatrix[Pixel] = NonEmptyMatrix(
+      rows = ps.rows.flatMap(r => if (r.forall(_ == EmptySpace)) NonEmptyList.of(r, r) else NonEmptyList.one(r))
+    )
--- src/test/scala/Day11Suite.scala
+++ src/test/scala/Day11Suite.scala
@@ -1,14 +1,117 @@
+import cats.data.NonEmptyList
 import munit.ScalaCheckSuite
 import org.scalacheck.Gen
 import org.scalacheck.Prop.*
 import Day11.*
+import Day11.Pixel.*
 import Day11Suite.*
 
 class Day11Suite extends ScalaCheckSuite:
 
-  test("day11 == 42"):
-    assertEquals(day11, 42)
+  test("non-empty matrix clockwise rotation"):
+    val m = NonEmptyMatrix(
+      rows = NonEmptyList.of(
+        NonEmptyList.of(1, 2),
+        NonEmptyList.of(3, 4),
+        NonEmptyList.of(5, 6)
+      )
+    )
+    assertEquals(
+      m.rotatedCW,
+      NonEmptyMatrix(
+        rows = NonEmptyList.of(
+          NonEmptyList.of(5, 3, 1),
+          NonEmptyList.of(6, 4, 2)
+        )
+      )
+    )
+
+  property("rotating CW and then CCW does NOT change any non-empty matrix"):
+    forAll(nonEmptyMatrixGen(Gen.alphaChar))(m => assertEquals(m.rotatedCW.rotatedCCW, m))
+
+  property("rotating CCW and then CW does NOT change any non-empty matrix"):
+    forAll(nonEmptyMatrixGen(Gen.alphaChar))(m => assertEquals(m.rotatedCCW.rotatedCW, m))
+
+  property("rotating CW four times does NOT change any non-empty matrix"):
+    forAll(nonEmptyMatrixGen(Gen.alphaChar))(m => assertEquals(m.rotatedCW.rotatedCW.rotatedCW.rotatedCW, m))
+
+  property("rotating CCW four times does NOT change any non-empty matrix"):
+    forAll(nonEmptyMatrixGen(Gen.alphaChar))(m => assertEquals(m.rotatedCCW.rotatedCCW.rotatedCCW.rotatedCCW, m))
+
+  test("non-empty matrix zip-with-position"):
+    assertEquals(
+      NonEmptyMatrix(
+        rows = NonEmptyList.of(
+          NonEmptyList.of('a', 'b', 'c'),
+          NonEmptyList.of('d', 'e', 'f')
+        )
+      ).zipWithPos,
+      NonEmptyMatrix(
+        rows = NonEmptyList.of(
+          NonEmptyList.of(('a', Pos(0, 0)), ('b', Pos(0, 1)), ('c', Pos(0, 2))),
+          NonEmptyList.of(('d', Pos(1, 0)), ('e', Pos(1, 1)), ('f', Pos(1, 2)))
+        )
+      )
+    )
+
+  test("parsing really small image successfully"):
+    assertEquals(
+      Image.parse(
+        input = List(
+          ".#.",
+          "...",
+          "..#"
+        )
+      ),
+      Some(
+        Image(
+          rows = NonEmptyList.of(
+            NonEmptyList.of(EmptySpace, Galaxy, EmptySpace),
+            NonEmptyList.of(EmptySpace, EmptySpace, EmptySpace),
+            NonEmptyList.of(EmptySpace, EmptySpace, Galaxy)
+          )
+        )
+      )
+    )
+
+  test("expanded small input 1"):
+    assertEquals(Image.parse(smallInput1).map(_.expanded), Image.parse(expandedSmallInput1))
 
 object Day11Suite:
 
   val bigInput: List[String] = getLinesFromFile("src/test/scala/day11_input.txt")
+
+  val smallInput1: List[String] = List(
+    "...#......",
+    ".......#..",
+    "#.........",
+    "..........",
+    "......#...",
+    ".#........",
+    ".........#",
+    "..........",
+    ".......#..",
+    "#...#....."
+  )
+  val expandedSmallInput1: List[String] = List(
+    "....#........",
+    ".........#...",
+    "#............",
+    ".............",
+    ".............",
+    "........#....",
+    ".#...........",
+    "............#",
+    ".............",
+    ".............",
+    ".........#...",
+    "#....#......."
+  )
+
+  def nonEmptyMatrixGen[A](aGen: Gen[A]): Gen[NonEmptyMatrix[A]] =
+    Gen.zip(Gen.choose(1, 10), Gen.choose(1, 10)).flatMap((w, h) => nonEmptyMatrixGen(aGen, w, h))
+  def nonEmptyMatrixGen[A](aGen: Gen[A], width: Int, height: Int): Gen[NonEmptyMatrix[A]] =
+    nonEmptyListGen(nonEmptyListGen(aGen, width), height).map(NonEmptyMatrix.apply)
+
+  def nonEmptyListGen[A](aGen: Gen[A], length: Int): Gen[NonEmptyList[A]] =
+    Gen.zip(aGen, Gen.listOfN(length - 1, aGen)).map(NonEmptyList.apply)
